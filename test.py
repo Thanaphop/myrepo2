68f7d79aa2c7b6699a7da79303f91fe1dbd93748
Thanaphop <th.thanaphop_st@tni.ac.th>
2015-12-01 14:01:45 +0700

    test 2nd commit

--- test.py
+++ test.py
@@ -14,3 +14,13 @@ print ( japaneseFruits )
 
 for x in allFruits :
     print( x )
+
+import datetime
+now = datetime.datetime.now()
+print( now )
+months = ('-error-', 'January','February','March','April','May','June', 'July','August','September','October','November','  December')
+print( months[now.month] )
+
+for x in months:
+    print x
+
